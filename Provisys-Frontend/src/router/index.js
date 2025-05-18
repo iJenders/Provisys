7f8fa5da83002ec2df38cbdc296e8382137cd2d0
iJenders <jenders2906@gmail.com>
2025-05-18 11:13:51 -0400

    Vistas por programar

--- Provisys-Frontend/src/router/index.js
+++ Provisys-Frontend/src/router/index.js
@@ -24,6 +24,63 @@ const router = createRouter({
       name: "contact",
       component: () => import("../views/ContactView.vue"),
     },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      component: () => import("../views/LogoutView.vue"),
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: () => import("../views/ProfileView.vue"),
+    },
+    {
+      path: "/system",
+      name: "system",
+      component: () => import("../views/SystemView.vue"),
+      children: [
+        {
+          path: "summary",
+          name: "system-summary",
+          component: () => import("../views/System/SummaryView.vue"),
+        },
+        {
+          path: "orders",
+          name: "system-orders",
+          component: () => import("../views/System/OrdersView.vue"),
+        },
+        {
+          path: "products",
+          name: "system-products",
+          component: () => import("../views/System/ProductsView.vue"),
+        },
+        {
+          path: "inventory",
+          name: "system-inventory",
+          component: () => import("../views/System/InventoryView.vue"),
+        },
+        {
+          path: "customers",
+          name: "system-customers",
+          component: () => import("../views/System/CustomersView.vue"),
+        },
+        {
+          path: "reports",
+          name: "system-reports",
+          component: () => import("../views/System/ReportsView.vue"),
+        },
+      ],
+    },
   ],
 });
 
---
+++ Provisys-Frontend/src/views/SystemView.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        System View
+    </h1>
+</template>
