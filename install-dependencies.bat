527712473462f87fffdefbfe200626c00de273a6
Jenderson Abarca <jenders2906@gmail.com>
2025-06-21 11:49:58 -0400

    Se añaden launchers para desarrollo

---
+++ install-dependencies.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo Instalando dependencias...
+
+cd "Provisys-Frontend"
+CALL npm install
+
+echo.
+echo npm install ha finalizado en Provisys-Frontend.
+echo Presiona cualquier tecla para salir...
+
+PAUSE
